d16f9a952c6e1a155b3cd26b04e7384398c2869d
henri <henri.rapeli@gmail.com>
2024-01-13 14:19:48 +0200

    Finished (at least a first version) of get_nuclides

--- main.py
+++ main.py
@@ -31,14 +31,15 @@ def _get_daughters(decay_data: list[dict]) -> list:
     return daughters
 
 
-def get_nuclides(src_nuclei: str, data_handler: DataHandler) -> None:
+def get_nuclides(src_nuclide: str, data_handler: DataHandler) -> list[Nuclide]:
     """
-    :param src_nuclei:
+    :param src_nuclide:
     :param data_handler:
     :return:
     """
-    stack = [src_nuclei]
-    nuclides = []
+    stack = [src_nuclide]
+    nuclide_objs = {}
+    nuclide_dict = {}
     while stack:
         nuc = stack.pop()
         logging.info(msg=f"Found nuclide: {nuc}")
@@ -50,16 +51,33 @@ def get_nuclides(src_nuclei: str, data_handler: DataHandler) -> None:
         halflife = nuc_data["half_life_sec"]
         if not halflife:
             halflife = None
-        nuclides.append(Nuclide(sym=sym, n=n, z=z, halflife=halflife, atom_mass=mass))
+        if nuc not in nuclide_dict.keys():
+            nuclide_objs[nuc] = Nuclide(sym=sym, n=n, z=z, halflife=halflife, atom_mass=mass)
+            nuclide_dict[nuc] = {"parents": set(), "daughters": set()}
         for daughter in _get_daughters(nuc_data["decays"]):
+            nuclide_dict[nuc]["daughters"].add(daughter)
             stack.append(daughter)
+
+    # Fill the parent sets as well
+    for parent, inner_dict in nuclide_dict.items():
+        for daughter in inner_dict["daughters"]:
+            nuclide_dict[daughter]["parents"].add(parent)
+
+    nuclide_lst = []
+    # Add the parents and daughters to the Nuclide objects
+    for nuclide, inner_dict in nuclide_dict.items():
+        for parent, daughter, in zip(inner_dict["parents"], inner_dict["daughters"]):
+            nuclide_objs[nuclide].sources.append(nuclide_objs[daughter])
+            nuclide_objs[nuclide].sinks.append(nuclide_objs[parent])
+        nuclide_lst.append(nuclide_objs[nuclide])
+    return nuclide_lst
 
 
 def main() -> None:
-    src_nuclei = "u238"
+    src_nuclide = "xe135"
     csv_path = "livechart.csv"
     data_handler = DataHandler(input_csv_path=csv_path)
-    get_nuclides(src_nuclei=src_nuclei, data_handler=data_handler)
+    nuclides = get_nuclides(src_nuclide=src_nuclide, data_handler=data_handler)
 
 
 if __name__ == "__main__":
