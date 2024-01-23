d41e5f0c36a23543d03862a4ca0cde01afe1a9cc
henri <henri.rapeli@gmail.com>
2024-01-23 21:18:46 +0200

    Added a Decay class

---
+++ decay.py
@@ -0,0 +1,39 @@
+"""
+File for a decay object, meant to represent a radioactive decay event or some shit
+"""
+
+import nuclide
+
+
+class Decay:
+    def __init__(self, parent: nuclide.Nuclide, daughter: nuclide.Nuclide,
+                 lamda: int | float, decay_ratio: float) -> None:
+        """
+        :param parent:
+        :param daughter:
+        :param lamda:
+        :param decay_ratio:
+        """
+        self.parent = parent
+        self.daughter = daughter
+        self.lamda = lamda
+        self.decay_ratio = decay_ratio
+        self.lamda_eff = lamda * decay_ratio  # "Effective" decay coefficient [1/s]
+
+    def calculate(self) -> int | float:
+        """
+        :return:
+        """
+        return self.lamda_eff * self.parent.n
+
+    def __repr__(self) -> str:
+        """
+        :return:
+        """
+        return f"{self.__class__.__name__}({self.parent} -> {self.daughter})"
+
+    def __str__(self) -> str:
+        """
+        :return:
+        """
+        return self.__repr__()
